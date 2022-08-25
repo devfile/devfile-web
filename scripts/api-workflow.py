fe981d15babf8ea45347a6f012d3e6efbf2f85b9
Paul Schultz <pschultz@pobox.com>
2022-08-25 17:22:14 -0500

    refactoring api-workflow
    Signed-off-by: Paul Schultz <pschultz@pobox.com>

--- scripts/api-workflow.py
+++ scripts/api-workflow.py
@@ -5,90 +5,89 @@ import os
 import typing
 import shutil
 
-def get_version_breakdown(version: str, version_type: str) -> typing.Tuple[int, int, int]:
+config = {
+  'stable-version': 'libs/docs/src/config/stable-version.txt',
+  'versions': 'libs/docs/src/config/versions.txt',
+}
+
+
+def get_version_breakdown(new_version: str, version_type: str) -> typing.Tuple[int, int, int]:
   '''get the version breakdown'''
-  major, minor, bug_fix = re.search(r'^(\d+)\.(\d+)\.(\d+)', version).groups()
+  major, minor, bug_fix = re.search(r'^(\d+)\.(\d+)\.(\d+)', new_version).groups()
 
   if not (isinstance(major, str) and isinstance(minor, str) and isinstance(bug_fix, str)):
-    raise TypeError(f'{version_type} could not be read: {version}')
+    raise TypeError(f'{version_type} could not be read: {new_version}')
 
   return int(major), int(minor), int(bug_fix)
 
-def update_stable_versions(release: bool, version: str) -> typing.List[str]:
-  '''update stable-versions.txt'''
-  # Parse the schema version
-  major, minor, bug_fix = get_version_breakdown(version, 'Inputted version')
+def update_stable_versions(release: bool, new_version: str) -> str:
+  '''update stable-version.txt'''
+  # Parse the version
+  new_major_version, new_minor_version, new_bug_fix_version = get_version_breakdown(new_version, 'Inputted version')
 
   cwd = os.getcwd()
-  stable_versions: typing.List[str] = []
+  stable_version: typing.List[str] = []
 
-  with open(f'{cwd}/libs/docs/src/config/stable-versions.txt', encoding='UTF-8') as f:
-    for line in f.readlines():
-      line = line.strip()
-      line_major, line_minor, line_bug_fix = get_version_breakdown(line, 'stable-version')
+  with open(f'{cwd}/libs/docs/src/config/stable-version.txt', encoding='UTF-8') as f:
+    for version in f.readlines():
+      version = version.strip()
+      major_stable_version, minor_stable_version, line_bug_fix_stable_version = get_version_breakdown(version, 'Stable version')
 
-      # Cannot change a stable version unless its a new release
-      if release and (line_major is major) and (line_minor is minor):
-        # Cannot change a bug fix version unless its a newer version
-        if line_bug_fix < bug_fix:
-          is_bug_fix = True
-          stable_versions.append(version)
-        else:
-          raise RuntimeError(f'Version {version} needs to be a newer version than {line}')
-
+      # Only change the stable version if its a release
+      if not release:
+        stable_version = version
       else:
-        stable_versions.append(line)
+        # Cannot change a stable version unless its a newer release
+        if (major_stable_version > new_major_version) and (minor_stable_version > new_minor_version) and (line_bug_fix_stable_version >= new_bug_fix_version):
+          raise RuntimeError(f'Version {new_version} needs to be a newer version than {version}')
+
+        stable_version = new_version
 
   if release:
-    # New stable version
-    if not is_bug_fix:
-      stable_versions.append(version)
+    with open(f'{cwd}/libs/docs/src/config/stable-version.txt', 'w', encoding='UTF-8') as f:
+      f.write(f'{stable_version}\n\n')
 
-    with open(f'{cwd}/libs/docs/src/config/stable-versions.txt', 'w', encoding='UTF-8') as f:
-      f.write('\n'.join(stable_versions))
+  return stable_version
 
-  return stable_versions
-
-def update_versions(stable_versions: typing.List[str], version: str) -> typing.Tuple[typing.List[str], typing.Tuple[str, str] | None, typing.Tuple[str, str | None]]:
+def update_versions(stable_version: str, new_version: str) -> typing.Tuple[typing.List[str], typing.Tuple[str, str] | None]:
   '''update versions.txt'''
-  # Parse the schema version
-  major, minor, bug_fix = get_version_breakdown(version, 'Inputted version')
+  # Parse the version
+  new_major_version, new_minor_version, new_bug_fix_version = get_version_breakdown(new_version, 'Inputted version')
 
   cwd = os.getcwd()
   versions: typing.List[str] = []
-  version_change = None
-  version_release = None
+  renamed_version = None
 
   with open(f'{cwd}/libs/docs/src/config/versions.txt', encoding='UTF-8') as f:
-    for line in f.readlines():
-      line = line.strip()
-      version_release = (version, None)
-      line_major, line_minor, line_bug_fix = get_version_breakdown(line, 'Version')
+    for version in f.readlines():
+      version = version.strip()
+      major_version, minor_version, bug_fix_version = get_version_breakdown(version, 'Version')
 
-      if (line_major is major) and (line_minor is minor):
-        # Cannot change a stable version unless its a new release
-        # Cannot change a bug fix version unless its a newer version
-        if line_bug_fix < bug_fix:
-          if line in stable_versions:
-            raise RuntimeError(f'Version {version} is a stable version and cannot be changed unless the argument "--release" is passed')
-
-          version_change = (line, version)
-          versions.append(version)
-        else:
-          raise RuntimeError(f'Version {version} needs to be a newer version than {line}')
-
+      # If the new major and minor versions are the same, then we need to update the bug fix version
+      if (major_version is not new_major_version) and (minor_version is not new_minor_version):
+        versions.append(version)
       else:
-        versions.append(line)
+        # Cannot change a bug fix version unless its a newer version
+        if bug_fix_version > new_bug_fix_version:
+          raise RuntimeError(f'Version {new_version} needs to be a newer version than {version}')
+
+        major_stable_version, minor_stable_version, bug_fix_stable_version = get_version_breakdown(stable_version, 'Stable version')
+
+        # Cannot change a stable version unless its a newer release
+        if (major_stable_version > new_major_version) and (minor_stable_version > new_minor_version) and (bug_fix_stable_version >= new_bug_fix_version):
+          raise RuntimeError(f'Version {new_version} is a stable version and cannot be changed unless the argument "--release" is passed')
+
+        renamed_version = (version, new_version)
+        versions.append(new_version)
 
   # New version
-  if not version_change:
-    version_release = (versions[-1], version)
-    versions.append(version)
+  if not renamed_version:
+    versions.append(new_version)
 
   with open(f'{cwd}/libs/docs/src/config/versions.txt', 'w', encoding='UTF-8') as f:
     f.write('\n'.join(versions))
 
-  return versions, version_change, version_release
+  return versions, renamed_version
 
 def update_typescript(versions: typing.List[str], stable: str) -> None:
   '''update version.ts'''
@@ -102,50 +101,42 @@ export const docVersions = {versions} as const;
 export const defaultVersion: DocVersions = '{stable}';
 ''')
 
-def rename_directory(version_change: typing.Tuple[str, str]) -> None:
-  '''rename the directory after a version change'''
+def create_directory(version_change: typing.Tuple[str, str], is_renamed: bool = False) -> None:
+  '''create the directory after a version change'''
   previous_version, new_version = version_change
-  if previous_version is new_version:
-    return None
-
   cwd = os.getcwd()
 
-  os.rename(f'{cwd}/libs/docs/src/docs/{previous_version}', f'{cwd}/libs/docs/src/docs/{new_version}')
+  if is_renamed:
+    if previous_version is new_version:
+      return
 
-def create_directory(version_release: typing.Tuple[str, str]) -> None:
-  '''create the directory for the new version'''
-  previous_version, new_version = version_release
+    os.rename(f'{cwd}/libs/docs/src/docs/{previous_version}', f'{cwd}/libs/docs/src/docs/{new_version}')
+  else:
+    shutil.copytree(f'{cwd}/libs/docs/src/docs/{previous_version}', f'{cwd}/libs/docs/src/docs/{new_version}')
+  
+def create_devfile_schema(version_change: typing.Tuple[str, str], devfile_schema: str, is_renamed: bool = False) -> None:
+  '''create the devfile schema after a version change'''
+  previous_version, new_version = version_change
   cwd = os.getcwd()
 
-  shutil.copytree(f'{cwd}/libs/docs/src/docs/{previous_version}', f'{cwd}/libs/docs/src/docs/{new_version}')
-
-def create_devfile_schema(versions: typing.Tuple[str, str], devfile_schema: str, delete_old_devfile_schema: bool = False) -> None:
-  '''create the devfile schema'''
-  previous_version, new_version = versions
-  cwd = os.getcwd()
-
-  if delete_old_devfile_schema:
+  if is_renamed:
     os.remove(f'{cwd}/libs/docs/src/devfile-schemas/{previous_version}.json')
 
   with open(f'{cwd}/libs/docs/src/devfile-schemas/{new_version}.json', 'w', encoding='UTF-8') as f:
     f.write(devfile_schema)
 
-def rename_navigation(version_change: typing.Tuple[str, str]) -> None:
+def create_navigation(version_change: typing.Tuple[str, str], is_renamed: bool = False) -> None:
   '''rename the navigation after a version change'''
   previous_version, new_version = version_change
-  if previous_version is new_version:
-    return None
-
   cwd = os.getcwd()
 
-  os.rename(f'{cwd}/libs/docs/src/navigation/{previous_version}.yaml', f'{cwd}/libs/docs/src/navigation/{new_version}.yaml')
+  if is_renamed:
+    if previous_version is new_version:
+      return None
 
-def create_navigation(version_release: typing.Tuple[str, str]) -> None:
-  '''create the navigation for the new version'''
-  previous_version, new_version = version_release
-  cwd = os.getcwd()
-
-  shutil.copy2(f'{cwd}/libs/docs/src/navigation/{previous_version}.yaml', f'{cwd}/libs/docs/src/navigation/{new_version}.yaml')
+    os.rename(f'{cwd}/libs/docs/src/navigation/{previous_version}.yaml', f'{cwd}/libs/docs/src/navigation/{new_version}.yaml')
+  else:
+    shutil.copy2(f'{cwd}/libs/docs/src/navigation/{previous_version}.yaml', f'{cwd}/libs/docs/src/navigation/{new_version}.yaml')
 
 def main():
   '''main method'''
@@ -163,26 +154,27 @@ def main():
   devfile_schema: str = args.devfile_schema
   release: str = args.release
 
-  # Update libs/docs/src/config/stable-versions.txt
-  stable_versions = update_stable_versions(release, version)
+  # Update libs/docs/src/config/stable-version.txt
+  stable_version = update_stable_versions(release, version)
 
   # Update libs/docs/src/config/versions.txt
-  versions, version_change, version_release = update_versions(stable_versions, version)
+  versions, renamed_version = update_versions(stable_version, version)
 
-  # Update the typescript in libs/docs/src/config/version.ts with the latest stable version
-  update_typescript(versions, stable_versions[-1])
+  # Update the typescript in libs/docs/src/config/version.ts
+  update_typescript(versions, stable_version)
 
-  # Rename the directory if a version change was made
-  if version_change:
-    rename_directory(version_change)
-    create_devfile_schema(version_change, devfile_schema, delete_old_devfile_schema=True)
-    rename_navigation(version_change)
-
+  # Rename the directories; otherwise, create the new directories
+  if renamed_version:
+    create_directory(renamed_version, is_renamed=True)
+    create_devfile_schema(renamed_version, devfile_schema, is_renamed=True)
+    create_navigation(renamed_version, is_renamed=True)
   # Create the directory if there is a new version
-  if version_release[-1]:
-    create_directory(version_release)
-    create_devfile_schema(version_release, devfile_schema)
-    create_navigation(version_release)
+  else:
+    create_directory(tuple(versions[-2:]))
+    create_devfile_schema(tuple(versions[-2:]), devfile_schema)
+    create_navigation(tuple(versions[-2:]))
+
+  print(config)
 
 if __name__ == '__main__':
   main()
