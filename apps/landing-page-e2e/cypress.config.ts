106d31be791484da4dcf8b8175be404e59e3466e
Paul Schultz <pschultz@pobox.com>
2022-10-10 14:58:04 -0500

    fixed e2e tests
    Signed-off-by: Paul Schultz <pschultz@pobox.com>

--- apps/landing-page-e2e/cypress.config.ts
+++ apps/landing-page-e2e/cypress.config.ts
@@ -14,4 +14,9 @@
  * limitations under the License.
  */
 
-describe('Registry Viewer', () => {});
+import { defineConfig } from 'cypress';
+import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
+
+export default defineConfig({
+  e2e: nxE2EPreset(__dirname),
+});
