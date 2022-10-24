1059e9e536ea8d87c76b0ce7c7f361727b8a6653
Michael Valdron <mvaldron@redhat.com>
2022-10-24 13:38:39 -0400

    NEXT_PUBLIC_DEVFILE_REGISTRIES env variable added to build scripts and CI. Push image workflow uses build_viewer.sh script for building the docker image.
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>

--- scripts/build_viewer.sh
+++ scripts/build_viewer.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 docker build -t registry-viewer . \
-            --build-arg PROJECT_NAME=registry-viewer \
-            --build-arg NEXT_PUBLIC_BASE_PATH=/viewer \
-            --build-arg NEXT_PUBLIC_ANALYTICS_WRITE_KEY=${NEXT_PUBLIC_ANALYTICS_WRITE_KEY:-""}
+    --build-arg PROJECT_NAME=registry-viewer \
+    --build-arg NEXT_PUBLIC_BASE_PATH=/viewer \
+    --build-arg NEXT_PUBLIC_DEVFILE_REGISTRIES="${NEXT_PUBLIC_DEVFILE_REGISTRIES:-""}" \
+    --build-arg NEXT_PUBLIC_ANALYTICS_WRITE_KEY=${NEXT_PUBLIC_ANALYTICS_WRITE_KEY:-""}
