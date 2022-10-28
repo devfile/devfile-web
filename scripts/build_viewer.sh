070b25b1556624a758e38c8797b9c5352085b2b2
Michael Valdron <mvaldron@redhat.com>
2022-10-28 15:48:18 -0400

    build args for registry viewer changed to match recent changes to Dockerfile.
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>

--- scripts/build_viewer.sh
+++ scripts/build_viewer.sh
@@ -16,6 +16,6 @@
 
 docker build -t registry-viewer . \
     --build-arg PROJECT_NAME=registry-viewer \
-    --build-arg NEXT_PUBLIC_BASE_PATH=/viewer \
-    --build-arg NEXT_PUBLIC_DEVFILE_REGISTRIES="${NEXT_PUBLIC_DEVFILE_REGISTRIES:-""}" \
+    --build-arg SITE_URL=${SITE_URL:-"https://registry.stage.devfile.io/viewer"} \
+    --build-arg NEXT_PUBLIC_BASE_PATH=${NEXT_PUBLIC_BASE_PATH:-"/viewer"} \
     --build-arg NEXT_PUBLIC_ANALYTICS_WRITE_KEY=${NEXT_PUBLIC_ANALYTICS_WRITE_KEY:-""}
